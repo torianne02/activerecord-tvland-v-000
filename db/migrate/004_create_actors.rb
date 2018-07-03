80f0e883abd1fceb342cb7fb35a0f17c6d7a213f
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-03 18:47:54 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,6 @@
 class CreateActors < ActiveRecord::Migration[4.2]
   def change
-    create_table :actors |a|
+    create_table :actors do |a|
       a.string :first_name
       a.string :last_name
     end
